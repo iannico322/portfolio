41175f7c654c92d845a9d2a728cd7859efe2b190
iannico322 <iannicocaulin@gmail.com>
2024-02-17 02:26:05 +0800

    sds

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,18 @@ function App() {
 
       
        
-        <div className="  fixed border border-b-2 border-accent flex justify-around items-center w-full py-3  animate__animated animate__fadeInDown bg-background/30 backdrop-blur-md z-10">
-          <Link to="/react-vite-supreme" className=" flex  items-center gap-10">
-            <h1 className=" text-primary text-4xl font-inter font-black leading-[30px] ">
+        <div className="  fixed border border-b-[1px] border-accent/60 flex justify-around items-center w-full py-3  animate__animated animate__fadeInDown bg-background/30 backdrop-blur-md z-10">
+          <Link to="/react-vite-supreme" className=" flex  items-center gap-10 sm:gap-8">
+            <h1 className=" text-primary text-4xl font-inter font-black leading-[30px] sm:leading-[20px] sm:text-2xl ">
               N <span className=" text-accent-foreground ml-[2px]">I</span> <br />{" "}
               <span>CO</span>{" "}
             </h1>
           </Link>
 
-          <nav className=" text-accent-foreground flex gap-10">
-            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 ease-out transition-all duration-200 underline-offset-4 animate__animated animate__faster animate__fadeInDown animate__delay-1s	" href="/react-vite-supreme/page2">About</a>
-            <a className=" hover:underline hover:text-primary hover:decoration-primary/40  underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-2s" href="/react-vite-supreme/contact">Work</a>
-            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-3s" href="/react-vite-supreme/contact">Contact</a>
+          <nav className=" text-accent-foreground flex gap-10 sm:gap-5">
+            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 ease-out transition-all duration-200 sm:text-xs underline-offset-4 animate__animated animate__faster animate__fadeInDown animate__delay-1s	" href="/react-vite-supreme/page2">About</a>
+            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 sm:text-xs  underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-2s" href="/react-vite-supreme/contact">Work</a>
+            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 sm:text-xs underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-3s" href="/react-vite-supreme/contact">Contact</a>
           </nav>
         </div>
 
@@ -45,7 +45,7 @@ function App() {
 
       
 <div className=" absolute h-screen w-full overflow-hidden  ">
-<div className= " fixed gradient  z-1  bottom-0 right-0 m-[10%]  "></div>
+<div className= " absolute gradient  z-1  bottom-0 right-0 m-[10%]  "></div>
 </div>
 
 
@@ -57,7 +57,7 @@ function App() {
     
     
     <img
-      className=" md:scale-[1.3] pointer-events-none object-contain h-[1000px] animate-float "
+      className=" md:scale-[1.3] pointer-events-none object-contain h-[1000px] sm:h-[90vw] sm:bottom-0 sm:mt-[30vh]   "
       src={GojoProfile}
       alt=""
     />
@@ -65,9 +65,9 @@ function App() {
 
   
 
-  <div className=" relative flex h-[60%] flex-col justify-start gap-20">
+  <div className=" relative flex h-[60%] flex-col sm:justify-end gap-20  sm:w-full sm:ml-4 mt-60 ">
     <div className=" flex flex-col  gap-1">
-      <h1 className=" text-accent-foreground font-semibold text-[64px]  leading-[60px]  animate__animated animate__fadeInUp    ">
+      <h1 className=" text-accent-foreground font-semibold text-[64px]  leading-[60px] sm:text-[30px] sm:leading-[40px]  animate__animated animate__fadeInUp    ">
         Hi,
         <br />
         I’m{" "}
@@ -75,12 +75,12 @@ function App() {
           Nico
         </span>{" "}
       </h1>
-      <p className=" text-accent-foreground text-[24px] font-light leading-[25px] animate__animated animate__fadeInLeft animate__fast">
-        Front-end Developer and UI <br /> Designer
+      <p className=" text-accent-foreground text-[24px] font-light leading-[25px] animate__animated animate__fadeInLeft animate__fast sm:text-[11px] sm:leading-[15px]">
+        Front-end Developer and UI <br className=" sm:hidden" /> Designer
       </p>
       <Button
         size="icon"
-        className=" active:translate-x-2 hover:translate-x-2 transition-all duration-700   h-12 mt-7 items-center text-lg hover:bg-accent-foreground/70 bg-accent-foreground backdrop-blur-md  text-accent w-[40%] gap-1 rounded-lg "
+        className=" active:translate-x-2 hover:translate-x-2 transition-all duration-700   h-12 sm:h-10 sm:text-sm mt-7 sm:mt-2 items-center text-lg hover:bg-accent-foreground/70 bg-accent-foreground backdrop-blur-md  text-accent sm:w-[30%] w-[40%] gap-1 rounded-lg "
       >
        <span className=" mt-1 ml-2">Hire Me</span> 
 
@@ -88,7 +88,7 @@ function App() {
        
       </Button>
     </div>
-    <div className=" absolute bottom-0 flex flex-col gap-5">
+    <div className=" absolute sm:fixed sm:mb-4 bottom-0 flex flex-col sm:right-0 sm:mr-4 gap-5">
      
       <a href="" >
         <GitHubLogoIcon  className="   animate__animated animate__fadeInUp animate__faster animate__delay-.9s hover:text-accent-foreground/70  text-lg text-accent-foreground h-8 w-8 " />
@@ -106,16 +106,16 @@ function App() {
 
   
 
-  <div className=" relative md:absolute flex h-[60%] flex-col justify-start gap-20">
-    <div className=" flex flex-col  gap-2">
-      <p className=" font-semibold text-xl text-primary animate__animated animate__fadeInUp animate__delay-.8s  ">Explore on</p>
-      <div className=" flex flex-col gap-8 ">
-        <h1 className="  animate__animated animate__fadeInUp animate__delay-.5s   text-accent-foreground font-semibold text-[34px]  leading-[38px] ">
+  <div className=" relative sm:absolute  flex h-[60%] flex-col justify-start gap-20 sm:gap-1 sm:right-0 mr-5 ">
+    <div className=" flex flex-col  gap-1">
+      <p className=" font-semibold text-xl text-primary animate__animated animate__fadeInUp animate__delay-.8s  sm:text-sm">Explore on</p>
+      <div className=" flex flex-col gap-8 sm:gap-2 ">
+        <h1 className="  animate__animated animate__fadeInUp animate__delay-.5s   text-accent-foreground font-semibold text-[34px]  leading-[38px] sm:text-base ">
           Based in Cagayan de Oro <br />
           i’m developer and <br />
           UI/UX designer.
         </h1>
-        <p className=" animate__animated animate__fadeInRight animate__delay-1s  text-accent-foreground text-[16px] font-light leading-[25px] ">
+        <p className=" animate__animated animate__fadeInRight animate__delay-1s  text-accent-foreground text-[16px] font-light leading-[25px] sm:text-xs ">
           Hey are looking for a Front-end or Designer <br /> to build your
           brand and grow you business?
           <br />
@@ -126,7 +126,7 @@ function App() {
 
       <p
        
-        className=" active:translate-y-2 animate__animated animate__fadeIn animate__delay-1s  hover:cursor-pointer hover:bg-primary/30 backdrop-blur-lg  hover:text-accent-foreground transition-all duration-200 font-inter  mt-7  text-lg flex items-center justify-center gap-2   text-primary w-[40%]  rounded-md py-2 "
+        className=" active:translate-y-2 animate__animated animate__fadeIn animate__delay-1s  hover:cursor-pointer hover:bg-primary/30 backdrop-blur-lg  hover:text-accent-foreground transition-all duration-200 font-inter  mt-7 sm:mt-3  text-lg flex items-center justify-center gap-2  sm:text-xs  text-primary w-[40%] sm:w-[60%]  rounded-md py-2 "
       >
         Download CV
         <DownloadIcon className=" mb-1   text-lg h-5 w-5 "/>
--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -3,11 +3,11 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-black ">
       <div className="container">
-        <div className="content">
-          <div className="content__container">
-            <p className="content__container__text">Hello,</p>
+        <div className="content ">
+          <div className="content__container ">
+            <p className="content__container__text text-[35px] flex items-center justify-center sm:mt-10 sm:text-[40px]">Hello,</p>
 
-            <ul className="content__container__list">
+            <ul className="content__container__list sm:text-sm p-0 ">
               <li className="content__container__list__item">World ! </li>
               <li className="content__container__list__item">I'm Ian Nico ! </li>
               <li className="content__container__list__item">
