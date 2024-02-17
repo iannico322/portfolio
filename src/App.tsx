f62c58c002be06a30bed83e6d51be3f510afa0f7
iannico322 <iannicocaulin@gmail.com>
2024-02-17 11:21:42 +0800

    adding project card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider"
 
-
+import AnimatedCursor from "react-animated-cursor"
 
 import { Link, Outlet } from "react-router-dom";
 import { ChevronRightIcon, GitHubLogoIcon,InstagramLogoIcon,LinkedInLogoIcon,DownloadIcon } from '@radix-ui/react-icons'
@@ -10,30 +10,37 @@ import { Button } from "./components/ui/button";
 
 import { useEffect } from "react";
 import './gradient.css'
+import ProjectCard from "./components/card/card";
 function App() {
 
   useEffect(()=>{
     localStorage.setItem("vite-ui-theme","dark")
   },[])
-
+ 
 
   return (
       <ThemeProvider>
-      <div className=" flex flex-col bg-background min-h-screen w-full overflow-x-hidden box-border  ">
+     
+      <div className=" text-[#dadada87] flex flex-col bg-background min-h-screen w-full overflow-x-hidden box-border  ">
 
+      <AnimatedCursor
+      color='243,243,243'
+     
       
+    />
+    
        
         <div className="  fixed border border-b-[1px] border-accent/60 flex justify-around items-center w-full py-3  animate__animated animate__fadeInDown bg-background/30 backdrop-blur-md z-10">
-          <Link to="/react-vite-supreme" className=" flex  items-center gap-10 sm:gap-8">
+          <a href="#Nico" className=" flex  items-center gap-10 sm:gap-8">
             <h1 className=" text-primary text-4xl font-inter font-black leading-[30px] sm:leading-[20px] sm:text-2xl ">
               N <span className=" text-accent-foreground ml-[2px]">I</span> <br />{" "}
               <span>CO</span>{" "}
             </h1>
-          </Link>
+          </a>
 
           <nav className=" text-accent-foreground flex gap-10 sm:gap-5">
             <a className=" hover:underline hover:text-primary hover:decoration-primary/40 ease-out transition-all duration-200 sm:text-xs underline-offset-4 animate__animated animate__faster animate__fadeInDown animate__delay-1s	" href="/react-vite-supreme/page2">About</a>
-            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 sm:text-xs  underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-2s" href="/react-vite-supreme/contact">Work</a>
+            <a className=" hover:underline hover:text-primary hover:decoration-primary/40 sm:text-xs  underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-2s" href="#Work">Work</a>
             <a className=" hover:underline hover:text-primary hover:decoration-primary/40 sm:text-xs underline-offset-4 ease-out transition-all duration-200 animate__animated animate__faster animate__fadeInDown animate__delay-3s" href="/react-vite-supreme/contact">Contact</a>
           </nav>
         </div>
@@ -44,16 +51,16 @@ function App() {
         <Outlet />
 
       
-<div className=" absolute h-screen w-full overflow-hidden  ">
-<div className= " absolute gradient  z-1  bottom-0 right-0 m-[10%]  "></div>
+
+        <div className="   flex absolute h-[120vh] w-full overflow-hidden    ">
+<div className= "  absolute gradient  z-1  bottom-0 right-0 m-[15%]   "></div>
 </div>
 
 
+<div id="Nico" className="relative h-screen w-full bg-background/0 backdrop-blur-lg flex justify-around items-center overflow-hidden  ">
 
-<div className="relative h-screen w-full bg-background/10 backdrop-blur-lg flex justify-around items-center overflow-hidden  ">
 
-
-  <div className=" absolute  z-0 animate__animated animate__fast  animate__fadeIn ">
+  <div className=" absolute  z-0 animate__animated animate__jackInTheBox animate__delay-1s ">
     
     
     <img
@@ -89,14 +96,13 @@ function App() {
       </Button>
     </div>
     <div className=" absolute sm:fixed sm:mb-4 bottom-0 flex flex-col sm:right-0 sm:mr-4 gap-5">
-     
-      <a href="" >
+      <a href="https://github.com/iannico322" target="_blank" >
         <GitHubLogoIcon  className="   animate__animated animate__fadeInUp animate__faster animate__delay-.9s hover:text-accent-foreground/70  text-lg text-accent-foreground h-8 w-8 " />
       </a>
-      <a href="" className=" ">
+      <a href="https://www.instagram.com/ianian_niconico/" target="_blank">
         <InstagramLogoIcon className=" animate__animated animate__fadeInUp animate__faster animate__delay-.9s hover:text-accent-foreground/70  text-lg text-accent-foreground h-8 w-8 " />
       </a>
-      <a href="" >
+      <a href="https://www.linkedin.com/in/ian-nico" target="_blank" >
         <LinkedInLogoIcon className="animate__animated animate__fadeInUp animate__faster animate__delay-.9s hover:text-accent-foreground/70  text-lg text-accent-foreground h-8 w-8 " />
       </a>
       
@@ -124,19 +130,24 @@ function App() {
 
       </div>
 
-      <p
+      <a href="./Google-Cloud-Skill-badges.pdf" download={true}
        
-        className=" active:translate-y-2 animate__animated animate__fadeIn animate__delay-1s  hover:cursor-pointer hover:bg-primary/30 backdrop-blur-lg  hover:text-accent-foreground transition-all duration-200 font-inter  mt-7 sm:mt-3  text-lg flex items-center justify-center gap-2  sm:text-xs  text-primary w-[40%] sm:w-[60%]  rounded-md py-2 "
+        className=" active:translate-y-2 animate__animated animate__fadeIn animate__delay-1s   hover:bg-primary/30 backdrop-blur-lg  hover:text-accent-foreground transition-all duration-200 font-inter  mt-7 sm:mt-3  text-lg flex items-center justify-center gap-2  sm:text-xs  text-primary w-[40%] sm:w-[60%]  rounded-md py-2 "
       >
         Download CV
         <DownloadIcon className=" mb-1   text-lg h-5 w-5 "/>
-      </p>
+      </a>
       
     </div>
   </div>
 </div>
 
-<div className="relative h-[100vh] w-full  flex justify-around items-center  flex-col bg-background/10 backdrop-blur-lg box-border  ">
+<div id="Work" className=" flex flex-row relative justify-center  min-h-[50vh] w-full bg-background/0 backdrop-blur-lg items-center   box-border  gap-10 justify-self-center flex-wrap overflow-hidden my-[70px]  ">
+
+  <ProjectCard/>
+  <ProjectCard/>
+  <ProjectCard/>
+  <ProjectCard/>
 </div>
 
 </div>
---
+++ src/components/card/card.tsx
@@ -0,0 +1,24 @@
+
+import './card.css'
+function ProjectCard() {
+  return (
+    <div className="ProjectCard">
+  <div className="main-content">
+    <div className="header">
+  <span>Article on</span>
+  <span>29-June-2023</span>
+</div>
+<p className="heading">Different ways to use CSS in React</p>
+<div className="categories">
+  <span>React</span>
+  <span>Css</span>
+</div>
+  </div>
+<div className="footer">
+  by Harsh Gupta
+</div>
+</div>
+  )
+}
+
+export default ProjectCard
